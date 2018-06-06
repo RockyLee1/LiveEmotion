a70d2298b6d9acd3279a9d1dadd3b90f24039d9f
linzuhan <linzuhanbobei@163.com>
2018-06-06 17:18:40 +0800

    Initial Commit

---
+++ BBLiveEmotion/BBLiveEmotion.h
@@ -0,0 +1,19 @@
+//
+//  BBLiveEmotion.h
+//  BBLiveEmotion
+//
+//  Created by linzuhan on 2018/6/6.
+//  Copyright © 2018年 linzuhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBLiveEmotion.
+FOUNDATION_EXPORT double BBLiveEmotionVersionNumber;
+
+//! Project version string for BBLiveEmotion.
+FOUNDATION_EXPORT const unsigned char BBLiveEmotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBLiveEmotion/PublicHeader.h>
+
+
